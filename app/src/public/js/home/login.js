20110be95bdfec1e291625aa7ac5da24086e5fb8
jinwood93 <jinwood93@github.io>
2021-03-09 17:16:53 +0900

    에러 추가

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,9 +12,7 @@ function login(){
         id: id.value,
         password: password.value
     };
-    console.log(req);
-    console.log(JSON.stringify(req));
-    
+
     fetch("/login", {
         method: "POST",
         headers:{
@@ -22,4 +20,15 @@ function login(){
         },
         body: JSON.stringify(req),
     })
+    .then((res) => res.json())
+    .then((res) =>{
+        if (res.success){
+            location.href = "/";
+        }else{
+            alert(res.msg);
+        }
+    })
+    .catch((err) => {
+        console.error(new Error("로그인 중 에러 발생"))
+    });
 }
