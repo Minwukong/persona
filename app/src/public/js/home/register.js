78b2e1c458333c22e33854c61692d1635107a970
jinwood93 <jinwood93@github.io>
2021-03-12 12:26:53 +0900

    회원가입 필수요소 처리 함

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -12,6 +12,9 @@ registerBtn.addEventListener("click",register);
 // 이 로직은 프로트 엔드에서만 처리해주면 되서 비밀번호 확인 필요 x
 function register(){
     if(!id.value) return alert("아이디를 입력하여 주세요")
+    if(!name.value) return alert("이름을 입력하여 주세요")
+    if(!password.value) return alert("비밀번호를 입력하여 주세요")
+    if(!confirmPassword.value) return alert("비밀번호를 확인해 주세요")
 
     if (password.value !== confirmPassword.value){
         return alert("비밀번호가 일치하지 않습니다")
