bb6be80dc7bd03573d1f83ab10e9b937f9631c7e
jinwood93 <jinwood93@github.io>
2021-03-12 11:45:13 +0900

    DB 연동

--- app/app.js
+++ app/app.js
@@ -2,7 +2,9 @@
 // 모듈
 const express = require("express");  
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 const app = express();
+dotenv.config();
 
 
 
--- app/src/models/User.js
+++ app/src/models/User.js
@@ -9,6 +9,7 @@ class User {
 
     async login() {
         const client = this.body
+        try {
         const { id, password } = await UserStorage.getUserInfo(client.id);
        
 
@@ -19,7 +20,11 @@ class User {
             return { success: false, msg: "비밀번호가 틀렸습니다" };
         }
         return { success: false, msg: "존재하지 않는 아이디 입니다" };
+        }catch (err){
+            return {success: false, msg: "존재하지 않는 아이디 입니다"};
+        }
     }
+
 
     async register(){
         const client = this.body;
@@ -27,7 +32,7 @@ class User {
             const response = await UserStorage.save(client);
             return response;
         } catch(err){
-            return { success: false, msg: err };
+            return { success: false, msg: "이미 존재하는 아이디입니다" };
         }
     }
 } 
